9c1a956c62fb5b946e0249bbeed32e5fe66102b4
Artem Streltsov <artemstreltsov@eduroam-int-dhcp-97-58-105.ucl.ac.uk>
2024-02-09 15:29:50 +0000

    refactor code

---
+++ src/editor.rs
@@ -0,0 +1,47 @@
+use std::io::{self, stdout};
+use termion::raw::IntoRawMode;
+use termion::event::Key;
+use termion::input::TermRead;
+
+pub struct Editor {
+    should_quit: bool,
+}
+
+impl Editor {
+    pub fn run(&mut self) {
+        let _stdout = stdout().into_raw_mode().unwrap();
+        
+        loop {
+            if let Err(error) = self.process_keypress() {
+                die(error);
+            }
+            if self.should_quit {
+                break;
+            }
+        }
+    }
+    pub fn default() -> Self {
+        Self {
+            should_quit: false,
+        }
+    }
+    fn process_keypress(&mut self) -> Result<(), std::io::Error> {
+        let pressed_key = self.read_key()?;
+        match pressed_key {
+            Key::Ctrl('q') => self.should_quit = true,
+            _ => ()
+        }
+        Ok(())
+    }
+    fn read_key(&mut self) -> Result<Key, std::io::Error> {
+        loop {
+            if let Some(key) = io::stdin().lock().keys().next() {
+                return key;
+            }
+        }
+    }
+}
+
+fn die(e: std::io::Error) {
+    panic!("{}", e);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,8 @@
-use std::io::{self, stdout, Read};
-use termion::raw::IntoRawMode;
+#! [warn(clippy::all, clippy::pedantic)]
+mod editor;
 
-fn to_ctrl_byte(c: char) -> u8 {
-    let byte = c as u8;
-    byte & 0b0001_1111
-}
-
-fn die(e: std::io::Error) {
-    panic!("{}", e);
-}
+use editor::Editor;
 
 fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
-
-    for b in io::stdin().bytes() {
-        match b {
-            Ok(b) => {
-                let c = b as char;
-
-                if c.is_control() {
-                    println!("{:?} \r", b);
-                } else {
-                    println!("{:?} ({})\r", b, c);
-                }
-
-                if b == to_ctrl_byte('q') {
-                    break;
-                }
-            }
-            Err(err) => die(err),
-        }
-    }
+    Editor::default().run();
 }
