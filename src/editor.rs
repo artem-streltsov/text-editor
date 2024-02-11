0f705c0822dad303a6089f51bc06f569a2c53cd5
Artem Streltsov <artemstreltsov@Artems-MacBook-Pro.local>
2024-02-11 20:57:04 +0000

    add feature to open files by filename

--- src/editor.rs
+++ src/editor.rs
@@ -1,8 +1,12 @@
+use crate::Document;
+use crate::Row;
 use crate::Terminal;
+use std::env;
 use termion::event::Key;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
+#[derive(Default)]
 pub struct Position {
     pub x: usize,
     pub y: usize
@@ -11,7 +15,8 @@ pub struct Position {
 pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
-    cursor_position: Position
+    cursor_position: Position,
+    document: Document
 }
 
 impl Editor {
@@ -29,16 +34,25 @@ impl Editor {
         }
     }
     pub fn default() -> Self {
+        let args: Vec<String> = env::args().collect();
+        let document = if args.len() > 1 {
+            let file_name = &args[1];
+            Document::open(&file_name).unwrap_or_default()
+        } else {
+            Document::default()
+        };
+
         Self {
             should_quit: false,
             terminal: Terminal::default().expect("Failed to initialize terminal"),
-            cursor_position: Position { x: 0, y: 0 }
+            cursor_position: Position::default(),
+            document
         }
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         Terminal::cursor_hide();
-        Terminal::cursor_position(&Position{ x: 0, y: 0 });
+        Terminal::cursor_position(&Position::default());
         if self.should_quit {
             Terminal::clear_screen();
             println!("Goodbye.\r");
@@ -65,11 +79,19 @@ impl Editor {
         }
         Ok(())
     }
+    pub fn draw_row(&self, row: &Row) {
+        let start = 0;
+        let end = self.terminal.size().width as usize;
+        let row = row.render(start, end);
+        println!("{}\r", row)
+    }
     fn draw_rows(&self) {
         let height = self.terminal.size().height;
-        for row in 0..height-1 {
+        for terminal_row in 0..height-1 {
             Terminal::clear_current_line();
-            if row == height / 3 {
+            if let Some(row) = self.document.row(terminal_row as usize) {
+                self.draw_row(row);
+            } else if self.document.is_empty() && terminal_row == height / 3 {
                 self.draw_welcome_message();
             } else {
                 println!("~\r");
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![warn(clippy::all, clippy::pedantic)]
+mod document;
 mod editor;
+mod row;
 mod terminal;
+pub use document::Document;
+pub use row::Row;
 use editor::Editor;
 pub use terminal::Terminal;
 pub use editor::Position;
